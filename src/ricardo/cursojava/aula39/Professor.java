5b1aebcbd634ce641fc98e473e10f337a21be9e3
ricardovinicius <rvaf@ic.ufal.br>
2023-08-24 19:04:56 -0300

    24/08 nti

---
+++ src/ricardo/cursojava/aula39/Professor.java
@@ -0,0 +1,28 @@
+package ricardo.cursojava.aula39;
+
+public class Professor extends Pessoa {
+
+	private double salario;
+	private String nomeCurso;
+	
+	public double getSalario() {
+		return salario;
+	}
+	public void setSalario(double salario) {
+		this.salario = salario;
+	}
+	public String getNomeCurso() {
+		return nomeCurso;
+	}
+	public void setNomeCurso(String nomeCurso) {
+		this.nomeCurso = nomeCurso;
+	}
+	
+	public double calcularSalarioLiquido(){
+		return 0;
+	}
+	
+	public void verificarAcesso() {
+		this.nomeVisibilidade = "foo";
+	}
+}
