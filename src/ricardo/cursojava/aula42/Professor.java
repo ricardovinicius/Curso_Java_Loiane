c23d8323fb91c713c72053433591505b10cbbcf2
ricardovinicius <rvaf@ic.ufal.br>
2023-08-25 08:55:28 -0300

    Abstract e Final 25/08

---
+++ src/ricardo/cursojava/aula42/Professor.java
@@ -0,0 +1,27 @@
+package ricardo.cursojava.aula42;
+
+public class Professor extends Pessoa {
+    private double salario;
+    private String nomeCurso;
+
+    public String getNomeCurso() {
+        return nomeCurso;
+    }
+    public void setNomeCurso(String nomeCurso) {
+        this.nomeCurso = nomeCurso;
+    }
+    public double getSalario() {
+        return salario;
+    }
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+    
+    public double calcularSalarioLiquido() {
+        return 0;
+    }
+    //@Override
+    public void imprimirEtiquetaEndereco() {
+        System.out.println("Endereco do Professor: " + this.getEndereco());
+    }
+}
---
+++ src/ricardo/cursojava/aula42/Teste.java
@@ -0,0 +1,14 @@
+package ricardo.cursojava.aula42;
+
+public class Teste {
+    public static void main(String[] args) {
+        // Pessoa pessoa = new Pessoa();
+        Pessoa aluno = new Aluno();
+        Pessoa professor = new Professor();
+
+        aluno.setEndereco("Rua 1");
+        // aluno.imprimirEtiquetaEndereco();
+
+        // Constantes.URL_BLOG = "Something";
+    }
+}
